597bf556aee6529b6a87435e2e3c21bd02edc438
Anjolaoluwa <adebayoanjola@gmail.com>
2022-07-14 12:11:46 +0200

    chore: add base scaffold for app

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,27 +4,58 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    'plugin:prettier/recommended',
+    '@vue/prettier/@typescript-eslint',
   ],
   parserOptions: {
+    sourceType: 'module',
     ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
   overrides: [
     {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
+      files: ['**/*.vue', '**/*.ts'],
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 2020,
+      },
+      plugins: ['import', 'import-alias'],
+      rules: {
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }],
+        'vue/component-name-in-template-casing': [
+          'error',
+          'kebab-case',
+          {
+            registeredComponentsOnly: false,
+            ignores: [],
+          },
+        ],
+        'import-alias/import-alias': [
+          'error',
+          {
+            relativeDepth: 0, // allows relative paths for colocated files (import FILE from "./FILE.js")
+            aliases: [{ alias: '~', matcher: '^src' }],
+          },
+        ],
+      },
+    },
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true,
       },
+      rules: {
+        'no-console': 'off',
+      },
     },
   ],
-};
+}
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
-const { defineConfig } = require("@vue/cli-service");
+var path = require('path')
+const { defineConfig } = require('@vue/cli-service')
+const vueSrc = './src'
 module.exports = defineConfig({
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: '@import "@/assets/scss/main.scss";',
+      },
+    },
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, vueSrc),
+      },
+    },
+  },
+  devServer: {},
   transpileDependencies: true,
-});
+})
