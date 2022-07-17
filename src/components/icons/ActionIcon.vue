f666000c186ab824a41cdcf1cc791663750278a2
Anjolaoluwa <adebayoanjola@gmail.com>
2022-07-17 22:45:01 +0200

    feat: add status icons

--- src/components/icons/ActionIcon.vue
+++ src/components/icons/ActionIcon.vue
@@ -1,5 +1,10 @@
 <template>
-  <div></div>
+  <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M6 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2Zm12 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2Zm-6 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2Z"
+      fill="#64787C"
+    />
+  </svg>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
---
+++ src/components/icons/CloseIcon.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path fill="currentColor" d="M2.065.454l13.89 13.89-1.112 1.11L.954 1.565z" />
+    <path fill="currentColor" d="M.954 14.343L14.844.454l1.11 1.11-13.889 13.89z" />
+  </svg>
+</template>
